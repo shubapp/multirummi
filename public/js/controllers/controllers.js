0b7671289c9b620ecc2c71a8f1f22843a033d109
Dean Shub <deanshub@gmail.com>
2014-07-30 20:07:14 +0300

    Add player html and css

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -11,6 +11,9 @@ controllers.playerController = function($scope, $location) {
 			$location.url('/player');
 		}
 	};
+	$scope.playerDeck = [{number:1, color:"black"},{number:2, color:"blue"},{number:3, color:"red"},{number:10, color:"yellow"},
+						 {number:0, color:"black"},{number:12, color:"blue"},{number:9, color:"yellow"},{number:11, color:"blue"},
+						 {number:2, color:"yellow"},{number:5, color:"red"},{number:6, color:"black"},{number:7, color:"black"},{number:7, color:"black"},{number:7, color:"black"},{number:7, color:"black"},{number:7, color:"black"},{number:7, color:"black"},{number:7, color:"black"},{number:7, color:"black"},{number:7, color:"black"}];
 };
 
 controllers.hostController = function($scope, $http, $location) {
